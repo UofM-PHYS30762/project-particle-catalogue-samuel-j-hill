aaa1ca00187a3567649614b3ace36ddc18f77240
samuel-j-hill <samuel.hill-3@student.manchester.ac.uk>
2024-05-04 13:45:24 +0100

    Four momentum sum, difference and dot product functions added

--- four_momentum.cpp
+++ four_momentum.cpp
@@ -4,7 +4,7 @@
 // Parameterised constructor
 FourMomentum::FourMomentum(double energy, double p_x, double p_y, double p_z)
 {
-  if(print_constructor_destructor) {std::cout<<"Parameterised four momentum constructor called. "<<std::endl;}
+  //if(print_constructor_destructor) {std::cout<<"Parameterised four momentum constructor called. "<<std::endl;}
   try
   {
     if(energy < 0)
@@ -26,6 +26,39 @@ FourMomentum::FourMomentum(double energy, double p_x, double p_y, double p_z)
   }
 }
 
+std::vector<double> sum(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2)
+{
+  std::vector<double> summed_four_momentum;
+  for(int i = 0; i < 4; i++)
+  {
+    summed_four_momentum.push_back((four_momentum_1.four_momentum_vector)[i] + (four_momentum_2.four_momentum_vector)[i]);
+  }
+  
+  return summed_four_momentum;
+}
+
+std::vector<double> difference(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2)
+{
+  std::vector<double> summed_four_momentum;
+  for(int i = 0; i < 4; i++)
+  {
+    summed_four_momentum.push_back((four_momentum_1.four_momentum_vector)[i] - (four_momentum_2.four_momentum_vector)[i]);
+  }
+  
+  return summed_four_momentum;
+}
+
+double dot_product(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2)
+{
+  double dot_product = four_momentum_1.four_momentum_vector[0] * four_momentum_2.four_momentum_vector[0]
+                       - four_momentum_1.four_momentum_vector[1] * four_momentum_2.four_momentum_vector[1]
+                       - four_momentum_1.four_momentum_vector[2] * four_momentum_2.four_momentum_vector[2]
+                       - four_momentum_1.four_momentum_vector[3] * four_momentum_2.four_momentum_vector[3];
+
+  return dot_product;
+}
+
+
 void FourMomentum::set_four_momentum_vector(double energy, double p_x, double p_y, double p_z)
 {
   try
@@ -46,5 +79,19 @@ void FourMomentum::set_four_momentum_vector(double energy, double p_x, double p_
     four_momentum_vector[1] = p_x;
     four_momentum_vector[2] = p_y;
     four_momentum_vector[3] = p_z;
+  }
+}
+
+double FourMomentum::get_invariant_mass() 
+{
+  double invariant_mass_squared =  std::pow(four_momentum_vector[0],2) - std::pow(four_momentum_vector[1],2)
+                                - std::pow(four_momentum_vector[2],2) - std::pow(four_momentum_vector[3],2);
+  if(invariant_mass_squared >= 0)
+  {
+    return sqrt(invariant_mass_squared);
+  }
+  else
+  {
+    throw std::invalid_argument("Energy squared must be bigger than sum of squared momenta. ");
   }
 }
--- four_momentum.h
+++ four_momentum.h
@@ -9,23 +9,27 @@ class FourMomentum
 {
 private:
   std::vector<double> four_momentum_vector{0,0,0,0};
-  bool print_constructor_destructor{false};
+  //bool print_constructor_destructor{false};
 
 public:
   // Default constructor
-  FourMomentum() {if(print_constructor_destructor) {std::cout<<"Default four momentum constructor called"<<std::endl;}}
+  FourMomentum() {};
   // Parameterised constructor
   FourMomentum(double energy, double p_x, double p_y, double p_z);
   // Destructor 
-  ~FourMomentum() {if(print_constructor_destructor) {std::cout<<"Four momentum destructor called"<<std::endl;}}
+  ~FourMomentum() {};
+  friend std::vector<double> sum(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2);
+  friend std::vector<double> difference(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2);
+  friend double dot_product(FourMomentum& four_momentum_1, FourMomentum& four_momentum_2);
 
   std::vector<double> get_four_momentum_vector() {return four_momentum_vector;}
-  double get_invariant_mass() {return sqrt(std::pow(four_momentum_vector[0],2) - std::pow(four_momentum_vector[1],2)
-                                            - std::pow(four_momentum_vector[2],2) - std::pow(four_momentum_vector[3],2));}
-  bool get_print_constructor_destructor_status() const {return print_constructor_destructor;}
+  double get_invariant_mass();
+  //bool get_print_constructor_destructor_status() const {return print_constructor_destructor;}
   
   void set_four_momentum_vector(double energy, double p_x, double p_y, double p_z);
-  void set_print_constructor_destructor_status(bool print_constructor_destructor_status) {print_constructor_destructor = print_constructor_destructor_status;}
+  //void set_print_constructor_destructor_status(bool print_constructor_destructor_status) {print_constructor_destructor = print_constructor_destructor_status;}
 };
+
+
 
 #endif
